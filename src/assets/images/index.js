eb22d24ddd885658eabd81c8b424a4edaa95293a
Sean <sdillon215@gmail.com>
2022-04-18 05:36:49 -0700

    section three style update, new projects added to array, links open in new tab

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,3 +5,6 @@ export const Jtree = require('./j-tree.jpg');
 export const DillonDash = require('./dillon-dash.png');
 export const MrNatural = require('./mrNatural.jpg');
 export const CityORox = require('./cityORox.jpg');
+export const Shop = require('./shop-shop.png');
+export const EzBud = require('./ez-budget.png');
+export const Book = require('./book.png');
--- src/components/ThirdPageScroll/index.js
+++ src/components/ThirdPageScroll/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactPageScroller from 'react-page-scroller';
-import { DillonDash } from '../../assets/images'
+import Link from '@mui/material/Link';
+import { 
+    DillonDash,
+    Shop,
+    EzBud,
+    Book
+ } from '../../assets/images'
 
 function ThirdPageScroll() {
     const projects = [
@@ -9,58 +15,65 @@ function ThirdPageScroll() {
             github: 'https://github.com/Sdillon215/dillon-employee-dashboard',
             url: 'https://dillon-dash.herokuapp.com/',
             img: DillonDash,
-            description: 'Employee dashboard bluh blah blah it does the stuff and the things and what not and other stuff'
+            description: 'This employee dashboard is a prototype for an application that will ultimately replace the archaic command line programs currently used for sale and purchase order input at my family\'s business Dillon Floral Corporation.',
+            tech: ['MongoDB', 'Express', 'React', 'Node', 'Material UI']
         },
         {
-            name: 'Dillon Floral Employee Dashboard',
-            github: 'https://github.com/Sdillon215/dillon-employee-dashboard',
-            url: 'https://dillon-dash.herokuapp.com/',
-            img: DillonDash,
-            description: 'Employee dashboard bluh blah blah it does the stuff and the things and what not and other stuff'
+            name: 'Shop-Shop',
+            github: 'https://github.com/Sdillon215/shop-shop',
+            url: 'https://limitless-reaches-14789.herokuapp.com/',
+            img: Shop,
+            description: 'Shop-Shop is an e-commerce site utilizing React\'s Context API to handle global state and Stripe to manage payments.',
+            tech: ['MongoDB', 'Express', 'React', 'Node', 'Stripe']
+            
         },
         {
-            name: 'Dillon Floral Employee Dashboard',
-            github: 'https://github.com/Sdillon215/dillon-employee-dashboard',
-            url: 'https://dillon-dash.herokuapp.com/',
-            img: DillonDash,
-            description: 'Employee dashboard bluh blah blah it does the stuff and the things and what not and other stuff'
+            name: 'Easy Budget',
+            github: 'https://github.com/Sdillon215/easy-budget',
+            url: 'https://ez-budget-1.herokuapp.com/',
+            img: EzBud,
+            description: 'Easy Budget is a convenient PWA that can be used to track your transactions with or without internet connection.',
+            tech: ['MongoDB', 'Express.js', 'Mongoose', 'IndexedDB']
         },
         {
-            name: 'Dillon Floral Employee Dashboard',
-            github: 'https://github.com/Sdillon215/dillon-employee-dashboard',
-            url: 'https://dillon-dash.herokuapp.com/',
-            img: DillonDash,
-            description: 'Employee dashboard bluh blah blah it does the stuff and the things and what not and other stuff'
+            name: 'Look 4 Book',
+            github: 'https://github.com/Sdillon215/look-4-book',
+            url: 'https://look-4-book.herokuapp.com/',
+            img: Book,
+            description: 'An application that enables users to search for and save books to their profile.',
+            tech: ['MongoDB', 'Express.js', 'React.js', 'Node.js', 'Google API']
         }
     ]
     return (
         <>
             <ReactPageScroller>
                 {projects.map((project) => (
-                    <section className="three-scroll-cont">
-                        <div>
-                            <div className="three-scroll-sect">
-                                <div className="proj-card">
-                                    <img src={project.img} alt={project.name} />
-                                    <h2>{project.name}</h2>
-                                </div>
+                    <section key={project.name} className="three-scroll-cont">
+                        <div className="three-scroll-sect">
+                            <div className="proj-card">
+                                <img src={project.img} alt={project.name} />
+                                <h2>{project.name}</h2>
                             </div>
-                            <div className="three-scroll-sect-bottom">
-                                <div className="info-card">
-                                    <h3>Purpose</h3>
-                                    <p>{project.description}</p>
-                                </div>
-                                <div className="info-card">
-                                    <h3>Tech</h3>
-                                    <li>This tech</li>
-                                    <li>That tech</li>
-                                    <li>other tech</li>
-                                </div>
-                                <div className="info-card">
-                                    <h3>Links</h3>
-                                    <p>{project.github}</p>
-                                    <p>{project.url}</p>
-                                </div>
+                        </div>
+                        <div className="three-scroll-sect-bottom">
+                            <div className="info-card">
+                                <h3>Tech</h3>
+                                {project.tech.map((tek) => (
+                                    <p key={tek}>{tek}</p>
+                                ))}
+                            </div>
+                            <div className="info-card">
+                                <h3>Purpose</h3>
+                                <p>{project.description}</p>
+                            </div>
+                            <div className="info-card">
+                                <h3>Links</h3>
+                                <Link target="_blank" rel="noreferrer noopener" href={project.github} color="inherit" underline="hover">
+                                    <p>Repo</p>
+                                </Link>
+                                <Link target="_blank" rel="noreferrer noopener" href={project.url} color="inherit" underline="hover">
+                                    <p>Deployed Site</p>
+                                </Link>
                             </div>
                         </div>
                     </section>
