5843dd45cd56aafdbe158c22104684d684691a67
Sean <sdillon215@gmail.com>
2022-04-21 01:46:56 -0700

    about text center/justified

--- src/components/SectionTwo/index.js
+++ src/components/SectionTwo/index.js
@@ -33,7 +33,7 @@ function SectionTwo(props) {
                             <h2 className='typed-out'>Hi. I'm Sean.</h2>
                         </div>
                         <div>
-                            <p className="fade-in-text">
+                            <p id="about-text" className="fade-in-text">
                                 I am a Full Stack Web Developer. I was drawn to this field of work by the ability to work remotely and the freedom that allows. My introduction to Web developement started with a boot camp through The University of Utah. As I am learning new skills and technologies I am finding a passion in the creative proccess of building web applications. This skill set gives me the freedom work from anywhere in the world which has given me the ability to pursue other passions such as climbing and traveling. These passions fuel each other and the result is awesome code and time spent in the most beautiful places in the world!
                             </p>
                         </div>
