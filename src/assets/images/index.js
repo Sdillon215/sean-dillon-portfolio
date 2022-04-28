4bb393aa64c0b61021b486bcf9414f1062f0ab70
Sean <sdillon215@gmail.com>
2022-04-27 21:51:03 -0700

    about section changed, animation taken out to fix sizing/text render/scroll bug

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,6 @@
-export const Portrait = require('./portrait-chess.jpg');
-export const OuterLimits = require('./outer-limits.jpg');
 export const Jtree = require('./j-tree.jpg');
 export const DillonDash = require('./dillon-dash.png');
-export const CityORox = require('./cityORox.jpg');
 export const Shop = require('./shop-shop.png');
 export const EzBud = require('./ez-budget.png');
 export const Book = require('./book.png');
+export const Gambler = require('./gambler.jpg');
