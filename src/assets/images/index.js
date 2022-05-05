10bbdbdaceb275b22bfa57458903cb439cedaf36
Sean <sdillon215@gmail.com>
2022-05-05 03:27:30 -0700

    Jurrasic Park is back online

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,4 @@
-export const Jtree = require('./j-tree.jpg');
 export const DillonDash = require('./dillon-dash.png');
 export const Shop = require('./shop-shop.png');
 export const EzBud = require('./ez-budget.png');
 export const Book = require('./book.png');
-export const Gambler = require('./gambler.jpg');
