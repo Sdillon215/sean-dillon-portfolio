93eb782d84e8e882af1a5796c5577b608ba6ccfe
Sean <sdillon215@gmail.com>
2022-05-11 19:39:55 -0700

    section one lazy load background

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,3 +2,4 @@ export const DillonDash = require('./dillon-dash.png');
 export const Shop = require('./shop-shop.png');
 export const EzBud = require('./ez-budget.png');
 export const Book = require('./book.png');
+export const Jtree = require('./j-tree.avif');
--- src/components/SectionOne/index.js
+++ src/components/SectionOne/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { BackgroundImage } from 'react-image-and-background-image-fade'
+import { Jtree } from '../../assets/images';
 import Button from '@mui/material/Button';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 const SectionOne = (props) => {
   const { handleDownClick } = props;
-
+ 
   return (
-    <section id="first-section">
-      <div id="home-title">
-        <h1>SEAN DILLON</h1>
-        <h2>Full Stack Dev | Climber | Desert Rat</h2>
-      </div>
-      <Button sx={{ marginTop: "auto", marginBottom: "0" }} onClick={handleDownClick}  aria-label="jump to next page">
-        <KeyboardDoubleArrowDownIcon sx={{ fontSize: '8vw' }} className='arrow bounce' />
-      </Button>
-    </section>
+    <BackgroundImage
+      useChild
+      src={Jtree}
+      width='1060px'
+      height='795px'
+      isResponsive
+      lazyLoad
+      transitionTime='1.2s'>
+      <section className="first-section">
+        <div id="home-title">
+          <h1>SEAN DILLON</h1>
+          <h2>Full Stack Dev | Climber | Desert Rat</h2>
+        </div>
+        <Button sx={{ marginTop: "auto", marginBottom: "0" }} onClick={handleDownClick} aria-label="jump to next page">
+          <KeyboardDoubleArrowDownIcon sx={{ fontSize: '8vw' }} className='arrow bounce' />
+        </Button>
+      </section>
+    </BackgroundImage>
   );
 }
 
